f420cd77bd8a94d7f53fcf45e652dd36c832768c
John Hawley <johnny@Johns-MBP.lan>
2024-10-09 04:59:13 -0700

    push aws etcX

--- server.js
+++ server.js
@@ -53,3 +53,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
